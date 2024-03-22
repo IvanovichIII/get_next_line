4f56a042dcdcd9732fae9d33d477724fced59306
IvanovichIII <119001961+IvanovichIII@users.noreply.github.com>
2024-03-22 01:31:57 +0100

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -12,78 +12,47 @@
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *theString)
+int	ft_strlen(const char *str)
 {
 	int	i;
 
 	i = 0;
-	while (theString[i])
+	while (*str)
+	{
+		str++;
 		i++;
+	}
 	return (i);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	int		sizetotal;
-	char	*res;
-	int		i;
-	int		j;
-
-	i = 0;
-	sizetotal = ft_strlen(s1) + ft_strlen(s2);
-	res = malloc(sizeof(char) * (sizetotal + 1));
-	if (!res || !s1 || !s2)
-		return (NULL);
-	while (s1[i] != 0)
-	{
-		res[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j] != 0)
-	{
-		res[i] = s2[j];
-		i++;
-		j++;
-	}
-	res[sizetotal] = 0;
-	return (res);
-}
-
-void	ft_bzero(void *s, size_t n)
-{
-	char	*str;
-	size_t	i;
-
-	str = (char *)s;
-	i = 0;
-	while (i < n)
-	{
-		str[i] = '\0';
-		i++;
+	@@ -67,31 +64,26 @@ void	ft_bzero(void *s, size_t n)
 	}
 }
 
-char	*ft_strchr(const char *string, int searchedChar )
+char	*ft_strchr(const char *str, int c)
 {
-	char	*str;
-
-	str = (char *)string;
-	while (*str != searchedChar && *str != 0)
+	while (*str)
+	{
+		if (*str == (char)c)
+		{
+			return ((char *)str);
+		}
 		str++;
-	if (*str == searchedChar)
-		return (str);
-	else
-		return (NULL);
+	}
+	if ((char)c == '\0')
+		return ((char *)str);
+	return (NULL);
 }
 
-void	*ft_calloc(size_t elementCount, size_t elementSize)
+void	*ft_calloc(size_t num, size_t size)
 {
-	char	*res;
+	void	*ptr;
 
-	res = malloc(elementSize * elementCount);
-	if (!res)
+	ptr = malloc(num * size);
+	if (!ptr)
 		return (NULL);
-	ft_bzero(res, elementSize * elementCount);
-	return (res);
-}
+	else
+	{
+		ft_bzero(ptr, (num * size));
+		return (ptr);
+	}
